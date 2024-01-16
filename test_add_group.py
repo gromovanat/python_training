b6d147302fe81df7d1fe0c0904f82ed46ae620af
gromovanat <gromova.nv88@gmail.com>
2024-01-16 20:15:30 +0200

    Создание вспомогательных методов, часть 2

--- test_add_group.py
+++ test_add_group.py
@@ -19,9 +19,20 @@ class TestAddGroup(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd)
         self.open_groups_page(wd)
-        #init group creation
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
+
+    def create_group(self, wd):
+        # init group creation
         wd.find_element_by_name("new").click()
-        #fill group form
+        # fill group form
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("Name Group")
@@ -31,20 +42,14 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").click()
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("footer test")
-        #submit group creation
+        # submit group creation
         wd.find_element_by_name("submit").click()
-        #return to groups page
-        wd.find_element_by_link_text("group page").click()
-        #logout
-        wd.find_element_by_link_text("Logout").click()
 
     def open_groups_page(self, wd):
-        # open groups page
         wd.find_element_by_link_text("groups").click()
         wd.get("http://localhost/addressbook/group.php")
 
     def login(self, wd):
-        # login
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
@@ -53,7 +58,6 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_xpath("//input[@value='Login']").click()
 
     def open_home_page(self, wd):
-        # open home page
         wd.get("http://localhost/addressbook/group.php")
 
     def is_element_present(self, how, what):
