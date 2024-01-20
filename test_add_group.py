447050eb98fd7395eb8f971786850154d99be894
gromovanat <gromova.nv88@gmail.com>
2024-01-20 21:57:33 +0200

    Доработка тестов, вынесли открытие homepage, возврат на homepage внутрь методов login и create_group

--- test_add_group.py
+++ test_add_group.py
@@ -9,6 +9,7 @@ import unittest, time, re
 import warnings
 from group import Group
 
+
 class TestAddGroup(unittest.TestCase):
 
     def setUp(self):
@@ -19,6 +20,7 @@ class TestAddGroup(unittest.TestCase):
         wd.get("http://localhost/addressbook/group.php")
 
     def login(self, wd, username, password):
+        self.open_home_page(wd)
         wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys(username)
@@ -31,6 +33,7 @@ class TestAddGroup(unittest.TestCase):
         wd.get("http://localhost/addressbook/group.php")
 
     def create_group(self, wd, group):
+        self.open_groups_page(wd)
         # init group creation
         wd.find_element_by_name("new").click()
         # fill group form
@@ -45,6 +48,7 @@ class TestAddGroup(unittest.TestCase):
         wd.find_element_by_name("group_footer").send_keys(group.footer)
         # submit group creation
         wd.find_element_by_name("submit").click()
+        self.return_to_groups_page(wd)
 
     def return_to_groups_page(self, wd):
         wd.find_element_by_link_text("group page").click()
@@ -54,20 +58,14 @@ class TestAddGroup(unittest.TestCase):
 
     def test_add_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, "admin", "secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="Name Group", header="Group header test", footer="footer test"))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def test_add_empty_group(self):
         wd = self.wd
-        self.open_home_page(wd)
         self.login(wd, "admin", "secret")
-        self.open_groups_page(wd)
         self.create_group(wd, Group(name="", header="", footer=""))
-        self.return_to_groups_page(wd)
         self.logout(wd)
 
     def is_element_present(self, how, what):
