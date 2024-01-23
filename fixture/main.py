da28ab698e1c413e5b0438a3ec6f7d695de568c9
gromovanat <gromova.nv88@gmail.com>
2024-01-23 23:09:26 +0200

    Создание класса MainHelper - общий список методов

---
+++ fixture/main.py
@@ -0,0 +1,26 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from fixture.session import SessionHelper
+from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
+
+
+class MainHelper:
+
+    def __init__(self):
+        self.wd = webdriver.Firefox()
+        self.wd.implicitly_wait(30)
+        self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
+
+
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/group.php")
+
+
+    def destroy(self):
+        self.wd.quit()
