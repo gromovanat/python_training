9d1840b7589fa9dd8bf4cd6c1f6fd835b98fea3a
gromovanat <gromova.nv88@gmail.com>
2024-01-23 19:41:41 +0200

    Выделен класс для работы с контактами ContactHelper, оптимизировала кол-во тестов

--- fixture/app_contact.py
+++ fixture/app_contact.py
@@ -13,24 +13,11 @@ class AppContact:
         self.contact = ContactHelper(self)
         self.session = SessionHelper(self)
 
+
     def open_home_page(self):
         wd = self.wd
         wd.get("http://localhost/addressbook/")
 
-    """def login(self, username, password):
-        wd = self.wd
-        self.open_home_page()
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()"""
-
-
-    #def logout(self):
-     #   wd = self.wd
-     #   wd.find_element_by_link_text("Logout").click()
 
     def destroy(self):
         self.wd.quit()
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -11,7 +11,6 @@ def app(request):
 
 
 def test_add_contact(app):
-
     app.session.login(username = "admin", password = "secret")
     app.contact.create_new_contact(Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", addres="Moskovskaya st 18-15", home="+74012555555", mobile="+79114830657", work="+74012666666", fax="+74012888888",
                                 email1="vanya@mail.ru", email2="vanya2@mail.ru", email3="vanya3@mail.ru", bday="1", bmonth="January", byear="2000"))
