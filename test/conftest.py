44e2979d54257b0bf8f143754e2ff52aed902fd3
gromovanat <gromova.nv88@gmail.com>
2024-01-23 22:51:46 +0200

    Общая фикстура, применение для Групп

---
+++ test/conftest.py
@@ -0,0 +1,15 @@
+import pytest
+from fixture.application import Application
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+
+
+
+
+
+
