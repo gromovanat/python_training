cf402a7818166834dace0b9b37769d25098a222f
gromovanat <gromova.nv88@gmail.com>
2024-01-29 01:05:13 +0200

    Добавила предусловие на проверку хотя бы 1 элемента для удаления/модификации

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -2,5 +2,7 @@ from model.contact import Contact
 
 
 def test_edit_first_contact(app):
+    if app.contact.count() == 0:
+        app.contact.create_new_contact(Contact(firstname="Test"))
     app.contact.edit_first_contact(Contact(firstname="Roman", middlename="Romanovich", lastname="Romanov", bday="10"))
 
