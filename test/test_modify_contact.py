65183d1b12439f6d493f0b3118c45144fe8bf248
gromovanat <gromova.nv88@gmail.com>
2024-02-04 14:36:30 +0200

    ДЗ-11 часть 3: Сравнение списков по объектам для test_add_contact и test_modify_contact)

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,7 +5,10 @@ def test_edit_first_contact(app):
     if app.contact.count() == 0:
         app.contact.create_new_contact(Contact(firstname="Test"))
     old_contacts = app.contact.get_contact_list()
-    app.contact.edit_first_contact(Contact(firstname="Roman", middlename="Romanovich", lastname="Romanov", bday="10"))
+    contact = Contact(firstname="Roman", middlename="Romanovich", lastname="Romanov", bday="10")
+    contact.id = old_contacts[0].id
+    app.contact.edit_first_contact(contact)
     new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
-
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
