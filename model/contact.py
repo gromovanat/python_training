41ac3ded4aecc74474f6a2b448664c82ae0eafc7
gromovanat <gromova.nv88@gmail.com>
2024-02-04 14:21:45 +0200

    ДЗ-11 часть 2: Сравнение списков по объектам для test_del_contact)

--- model/contact.py
+++ model/contact.py
@@ -1,7 +1,8 @@
 class Contact:
 
     def __init__(self, firstname=None, middlename=None, lastname=None, address=None, home=None, mobile=None,
-                           work=None, fax=None, email1=None, email2=None, email3=None, bday=None, bmonth=None, byear=None, id=None):
+                 work=None, fax=None, email1=None, email2=None, email3=None, bday=None, bmonth=None,
+                 byear=None, id=None):
         self.firstname = firstname
         self.middlename = middlename
         self.lastname = lastname
@@ -18,3 +19,8 @@ class Contact:
         self.byear = byear
         self.id = id
 
+    def __repr__(self):
+        return "%s:%s %s" % (self.id, self.firstname, self.lastname)
+
+    def __eq__(self, other):
+        return self.id == other.id and self.firstname == other.firstname and self.lastname == other.lastname
