7f80fa3183f57638a0cc0e328b898c8c4982306a
gromovanat <gromova.nv88@gmail.com>
2024-02-12 22:13:36 +0200

    ДЗ-15 Параметризовать тесты для создания групп и контактов

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,11 +1,43 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
+import pytest
+import random
+import string
 
 
-def test_add_contact(app):
+def random_string(prefix, maxlen):
+    symbols = string.ascii_letters + string.digits + " "*10
+    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+
+def random_digits_string(maxlen):
+    digits = string.digits
+    return "".join([random.choice(digits) for i in range(random.randrange(maxlen))])
+
+
+def random_letters_string(prefix, maxlen):
+    letters = string.ascii_letters + " "*2
+    return prefix + "".join([random.choice(letters) for i in range(random.randrange(maxlen))])
+
+
+def random_email():
+    symbols_for_mail_name = string.ascii_letters + string.digits + "." + "-" + "_"
+    generated_mail_name = ''.join([random.choice(symbols_for_mail_name) for i in range(random.randrange(1, 15))])
+    generated_domain_name = ''.join([random.choice(string.ascii_letters) for i in range(random.randrange(2, 10))])
+    generated_main_domain = ''.join([random.choice(string.ascii_letters) for i in range(random.randrange(2, 5))])
+    random_emails = f"{generated_mail_name}@{generated_domain_name}.{generated_main_domain}"
+    return random_emails
+
+
+testdata = [Contact(firstname=random_letters_string("firstname", 10), lastname=random_letters_string("lastname", 10),
+                    address=random_string("address", 30),
+                    home=random_digits_string(11), mobile=random_digits_string(11), work=random_digits_string(11),
+                                email1=random_email(), email2=random_email(), email3=random_email())]
+
+
+@pytest.mark.parametrize("contact", testdata, ids=[repr(x) for x in testdata])
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact(firstname="Ivan", middlename="Ivanovich", lastname="Ivanov", address="Moskovskaya st 18-15", home="+74012555555", mobile="+79114830657", work="+74012666666", fax="+74012888888",
-                                email1="vanya@mail.ru", email2="vanya2@mail.ru", email3="vanya3@mail.ru", bday="1", bmonth="January", byear="2000")
     app.contact.create_new_contact(contact)
     assert len(old_contacts) + 1 == app.contact.count()
     new_contacts = app.contact.get_contact_list()
